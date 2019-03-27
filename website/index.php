0b8f822384cdc099ada17dfa5566cf4c81deb1e1
Those45Ninjas <tom.parker1235@gmail.com>
2019-03-28 00:04:14 +0800

    Added some basic files for the website

---
+++ website/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GDU Minecraft Register</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<?php
+	include("private/welcome-message.html")
+	?>
+	<a href="">Login with Mixer</a>
+
+	<div class="modal"></div>
+</body>
+</html>
